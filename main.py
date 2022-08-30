971cec8a2b6765243837301c85768c2464ff0bd8
Vinícius Bruno <91799009+Vinnybrunn00@users.noreply.github.com>
2022-08-30 01:49:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from imap_tools import MailBox, AND
+from mytoken import PassXablau, TeleToken
+from gtts import gTTS
+import time, telebot
+
+bot = telebot.TeleBot(TeleToken())
+print('Connected!!!')
+
+user = 'YOUT_EMAIL'
+passwd = PassXablau()
+
+@bot.message_handler(commands=['start'])
+def AlertEmail(message):
+    bot.reply_to(message, 'Monitoring of new active emails!')
+    while True:
+        with MailBox('imap.gmail.com').login(user, passwd) as inbox:
+            for msg in inbox.fetch(AND(seen=False)):
+                alert = 'email.mp3'
+                gtts = gTTS(
+                    text=f'New email from {msg.from_}',
+                    lang='pt-br'
+                )
+                gtts.save(alert)
+                bot.send_voice(message.chat.id, open(f'{alert}', 'rb'))
+                time.sleep(1)
+
+bot.infinity_polling()
